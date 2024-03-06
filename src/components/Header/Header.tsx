28125d5de58e45f45e0548738bc8406364a3e8b4
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2024-03-06 23:13:43 +0530

    feat: integrated near wallet

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useMbWallet } from "@mintbase-js/react";
+import NearWallet from "../NearWallet/NearWallet";
 
 function Header() {
-  const { isConnected, selector, connect, activeAccountId } = useMbWallet();
-
-  const handleSignout = async () => {
-    const wallet = await selector.wallet();
-    return wallet.signOut();
-  };
-
-  const handleSignIn = async () => {
-    return connect();
-  };
-
   return (
     <>
       <div>
@@ -21,28 +10,7 @@ function Header() {
           <Link href={"/"}>
             <Image src="/images/logo.svg" alt="logo" width={40} height={100} />
           </Link>
-          <div>
-            {!isConnected ? (
-              <button
-                onClick={handleSignIn}
-                className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-700 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 "
-              >
-                Connect Wallet
-              </button>
-            ) : (
-              <div>
-                <p>You are connected as {activeAccountId}</p>
-                <div className="flex justify-center items-center mt-4">
-                  <button
-                    className="bg-white text-black rounded p-3 hover:bg-[#e1e1e1]"
-                    onClick={handleSignout}
-                  >
-                    Disconnect{" "}
-                  </button>
-                </div>
-              </div>
-            )}
-          </div>
+          <NearWallet />
         </div>
       </div>
     </>
---
+++ src/components/NearWallet/NearWallet.tsx
@@ -0,0 +1,47 @@
+import { useMbWallet } from "@mintbase-js/react";
+function NearWallet() {
+  const {
+    connect,
+    disconnect,
+    activeAccountId,
+    selector,
+    isConnected,
+    errorMessage,
+  } = useMbWallet();
+  // console.log("chainid", activeAccountId, "selector", selector, "isConnected");
+  const handleSignout = async () => {
+    const wallet = await selector.wallet();
+    return wallet.signOut();
+  };
+
+  const handleSignIn = async () => {
+    return connect();
+  };
+  return (
+    <>
+      <div>
+        {!isConnected ? (
+          <button
+            onClick={handleSignIn}
+            className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-700 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 "
+          >
+            Connect Wallet
+          </button>
+        ) : (
+          <div>
+            <div className="flex justify-center items-center mt-4">
+              <button
+                className="bg-white text-black rounded p-3 hover:bg-[#e1e1e1]"
+                onClick={handleSignout}
+              >
+                Disconnect{" "}
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default NearWallet;
