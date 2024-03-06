3b5e2ce1212d4005c92bf299acf3bc4f9733918a
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2024-03-07 02:07:31 +0530

    chore: added minor details for ui

--- src/views/Buyer/components/BuyerNFTList.tsx
+++ src/views/Buyer/components/BuyerNFTList.tsx
@@ -27,6 +27,13 @@ function BuyerNFTList({ subscriptions }: Props) {
               return <NFTBalanceCard key={_id} subscription={subscription} />;
             })}
           </div>
+          {(subscriptions?.length as number) === 0 ? (
+            <div className="my-24 flex flex-row gap-x-3 gap-y-3 flex-wrap justify-center pl-2">
+              No Subscriptions
+            </div>
+          ) : (
+            ""
+          )}
         </div>
         {subscriptions === undefined ? (
           <span className="loading loading-ring loading-lg text-white flex flex-row justify-center mx-auto"></span>
